ad4820897d1a9f78f16836d65e64fda7e2279913
My name <me@me>
2016-05-18 13:41:00 +0200

    First commit.

--- app/runner.py
+++ app/runner.py
@@ -12,25 +12,15 @@ import traceback
 
 from platform import dist # check Linux distribution
 script_dir = os.path.dirname(__file__) + "/scenarios/"
-#print (script_dir)
 
 def validate_pr(problem):
     distro = dist()[0]
-    # scenario_run = [ i for i in os.listdir(script_dir)
-    #                    for pr in problem
-    #                    if pr == i.split(".")[0] ] 
-
-
     scenario_run = []
     non_existent = []
     dir_files = os.listdir(script_dir)
-    #print(dir_files)
 
     nl = { i.split(".")[0]:i for i in dir_files }
-    #non_existent = [ i for i in dir_files if i not in nl ]
-    #print(nl)
-    #print(problem)
-# this is shitty code. Fix it to compare in a reliable way.
+
     for pr in problem:
         if pr in nl.keys():
             scenario_run.append(nl[pr])
@@ -61,15 +51,11 @@ def organizer(problem):
                # print(distrib)
                 continue
             else:
-                #print(distrib)
                 distrib = distrib.group().split("=")[1].lower()
-                distrib = re.sub(r'[\s*\"\']|[\s*\"\']$', '', distrib) #distrib.group().split("=")[1]
-                #print(distrib)
+                distrib = re.sub(r'[\s*\"\']|[\s*\"\']$', '', distrib) 
                 if distrib == distro:
-                    #print ("xxx", distrib)
                     scn_validated.append(scn)
                 else:
-                   # print(distrib)
                     click.secho("\n --- %s is not compatible with %s. Feel free to contribute making a proper plugin for that.\n" %( scn, distro), err=True, fg="yellow")  
     return scn_validated
 
@@ -85,7 +71,6 @@ def tbs_cli():
             help='Troubleshooting scenario to be loaded.'
              )
 
-#@click.option('--distro', default=dist()[0], help='Choose the OS to load the Troubleshooting scenarios.')
 def runit(problem):
     """Run Troubleshooting scenarios."""
     scenarios = "\n".join(problem)
@@ -117,16 +102,3 @@ def runit(problem):
             click.secho("Scenario %s successfully loaded\n" % scn, fg="green")
             #click.echo("-----------------------------------------")
 
-
-#tbs_cli.add_command(runit(problem))
-
-# CRIAR FUNC PARA USAR DENTRO DE runit
-
-#@click.option('--location', default="local", help='The person to greet.')
-
-#def distro(distro=dist()[0]):
-    #get to specific directory to distro.
-    #pass
-
-# Criar clean-up function to remove dupplicate scripts
-# manter por default o script em python e/ou o mais novo.
